d6f55bb95be7c17cad4ca69f80c1abaca72455aa
Danuar Wiranata <danuarwr@gmail.com>
2024-07-09 21:20:00 +0700

    Add adapter

--- app/src/main/java/com/example/tourism_app/adapter/DestinationAdapter.kt
+++ app/src/main/java/com/example/tourism_app/adapter/DestinationAdapter.kt
@@ -1,25 +1,31 @@
 package com.example.tourism_app.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tourism_app.data.model.Destination
+import com.example.tourism_app.databinding.ItemPlacesBinding
 
 class DestinationAdapter(private val destinationList : List<Destination>) : RecyclerView.Adapter<DestinationAdapter.DestinationViewHolder>() {
 
-    class DestinationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+    class DestinationViewHolder(val binding: ItemPlacesBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(destinations: Destination) {
+            binding.tvDestinationName.text = destinations.name
+            binding.tvDestinationLocation.text = destinations.location
+            binding.imgDestinationPhoto.setImageResource(destinations.image!!)
+        }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DestinationViewHolder {
-        TODO("Not yet implemented")
+        val binding = ItemPlacesBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DestinationViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: DestinationViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val destinations = destinationList[position]
+        holder.bind(destinations)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = destinationList.size
 
 }
