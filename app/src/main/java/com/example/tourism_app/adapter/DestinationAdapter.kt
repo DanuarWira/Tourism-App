e70b67d524d8988dd6d672db56be8e0dda02d7fc
Danuar Wiranata <danuarwr@gmail.com>
2024-07-09 21:06:08 +0700

    Slicing UI

---
+++ app/src/main/java/com/example/tourism_app/adapter/DestinationAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.tourism_app.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tourism_app.data.model.Destination
+
+class DestinationAdapter(private val destinationList : List<Destination>) : RecyclerView.Adapter<DestinationAdapter.DestinationViewHolder>() {
+
+    class DestinationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DestinationViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: DestinationViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
--- app/src/main/java/com/example/tourism_app/adapter/PlaceAdapter.kt
+++
@@ -1,4 +0,0 @@
-package com.example.tourism_app.adapter
-
-class PlaceAdapter {
-}
